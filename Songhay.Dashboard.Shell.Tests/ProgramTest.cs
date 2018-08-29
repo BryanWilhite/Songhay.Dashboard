5cdbcf118458dccbb95b1105a3cfceb1a412de34
Bryan Wilhite <rasx@kintespace.com>
2018-08-29 09:46:34 -0700

    :microscope: ShouldGenerateAppData() passes, resolves #9

--- Songhay.Dashboard.Shell.Tests/ProgramTest.cs
+++ Songhay.Dashboard.Shell.Tests/ProgramTest.cs
@@ -1,10 +1,10 @@
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using Songhay.Extensions;
 using Songhay.Models;
-using System.IO;
 
 namespace Songhay.Dashboard.Shell.Tests
 {
@@ -15,40 +15,62 @@ namespace Songhay.Dashboard.Shell.Tests
 
         [TestMethod]
         [TestProperty("appFile", @"ClientApp\src\assets\data\app.json")]
-        [TestProperty("indexFile", @"json\index.json")]
+        [TestProperty("codepenFile", @"Songhay.Feeds\Songhay.Feeds.Tests\data\codepen.json")]
+        [TestProperty("flickrFile", @"Songhay.Feeds\Songhay.Feeds.Tests\data\flickr.json")]
+        [TestProperty("githubFile", @"Songhay.Feeds\Songhay.Feeds.Tests\data\github.json")]
+        [TestProperty("studioFile", @"Songhay.Feeds\Songhay.Feeds.Tests\data\studio.json")]
         [TestProperty("serverMetadataFile", @"json\server-meta.json")]
         public void ShouldGenerateAppData()
         {
+            var root = this.TestContext.ShouldGetAssemblyDirectoryParent(this.GetType(), expectedLevels : 5);
             var projectDirectoryInfo = this.TestContext.ShouldGetProjectDirectoryInfo(this.GetType());
             var webProjectInfo = this.TestContext.ShouldGetConventionalProjectDirectoryInfo(this.GetType());
 
             #region test properties:
 
             var appFile = this.TestContext.Properties["appFile"].ToString();
-            appFile = Path.Combine(webProjectInfo.FullName, appFile);
+            appFile = webProjectInfo.FullName.ToCombinedPath(appFile);
             this.TestContext.ShouldFindFile(appFile);
 
-            var indexFile = this.TestContext.Properties["indexFile"].ToString();
-            indexFile = Path.Combine(projectDirectoryInfo.FullName, indexFile);
-            this.TestContext.ShouldFindFile(indexFile);
+            var codepenFile = this.TestContext.Properties["codepenFile"].ToString();
+            codepenFile = root.ToCombinedPath(codepenFile);
+            this.TestContext.ShouldFindFile(codepenFile);
+
+            var flickrFile = this.TestContext.Properties["flickrFile"].ToString();
+            flickrFile = root.ToCombinedPath(flickrFile);
+            this.TestContext.ShouldFindFile(flickrFile);
+
+            var githubFile = this.TestContext.Properties["githubFile"].ToString();
+            githubFile = root.ToCombinedPath(githubFile);
+            this.TestContext.ShouldFindFile(githubFile);
+
+            var studioFile = this.TestContext.Properties["studioFile"].ToString();
+            studioFile = root.ToCombinedPath(studioFile);
+            this.TestContext.ShouldFindFile(studioFile);
 
             var serverMetadataFile = this.TestContext.Properties["serverMetadataFile"].ToString();
-            serverMetadataFile = Path.Combine(projectDirectoryInfo.FullName, serverMetadataFile);
+            serverMetadataFile = projectDirectoryInfo.FullName.ToCombinedPath(serverMetadataFile);
             this.TestContext.ShouldFindFile(serverMetadataFile);
 
             #endregion
 
             var serverMetaRoot = "serverMeta";
-            var indexRoot = "index";
+            var feedsRoot = "feeds";
 
             var jO = JObject.Parse(File.ReadAllText(appFile));
 
+            this.TestContext.WriteLine($"writing {serverMetaRoot}...");
             var jO_serverMetadata = JObject.Parse(File.ReadAllText(serverMetadataFile));
-
-            var jA_index = JArray.Parse(File.ReadAllText(indexFile));
-
             jO[serverMetaRoot] = jO_serverMetadata;
-            jO[indexRoot] = jA_index;
+
+            var feeds = new [] { codepenFile, flickrFile, githubFile, studioFile };
+            feeds.ForEachInEnumerable(i =>
+            {
+                var fileName = Path.GetFileNameWithoutExtension(i);
+                this.TestContext.WriteLine($"writing {feedsRoot}/{fileName}...");
+                var jO_feed = JObject.Parse(File.ReadAllText(i));
+                jO[feedsRoot][fileName] = jO_feed;
+            });
 
             File.WriteAllText(appFile, jO.ToString());
         }
