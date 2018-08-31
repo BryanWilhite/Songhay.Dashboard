6a921045fab01a71ec411e16cfa203b2a006d1f8
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-08-30 18:06:53 -0700

    :tractor: :lipstick: added studio feed component, issue #20

--- Songhay.Dashboard/ClientApp/src/app/components/dashboard/studio-feed/studio-feed.component.ts
+++ Songhay.Dashboard/ClientApp/src/app/components/dashboard/studio-feed/studio-feed.component.ts
@@ -7,11 +7,38 @@ import { DashboardDataService } from '../../../services/dashboard-data.service';
     styleUrls: ['./studio-feed.component.scss']
 })
 export class StudioFeedComponent implements OnInit {
+    feedItems: { title: string; link: string }[];
+    feedTitle: string;
+
     constructor(private dashService: DashboardDataService) {}
 
     ngOnInit() {
         this.dashService.appDataLoaded.subscribe((feed: Map<string, any>) => {
-            console.log('yup:appDataLoaded');
+            const rawFeed: any = feed['studio'];
+            if (!rawFeed) {
+                console.log('the expected raw feed is not here');
+                return;
+            }
+
+            const channel: any = rawFeed['rss']['channel'];
+            if (!channel) {
+                console.log('the expected RSS channel is not here');
+                return;
+            }
+            this.feedTitle = channel['title'];
+
+            const channelItems: {}[] = rawFeed['rss']['channel']['item'];
+            if (!channelItems) {
+                console.log('the expected RSS channel root is not here');
+                return;
+            }
+            if (!channelItems.length) {
+                console.log('the expected RSS channel items are not here');
+                return;
+            }
+            this.feedItems = channelItems.map(item => {
+                return { title: item['title'], link: item['link'] };
+            });
         });
     }
 }
