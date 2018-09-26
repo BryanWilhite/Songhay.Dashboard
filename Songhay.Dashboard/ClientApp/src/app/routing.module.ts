2bf4d6562ee4335aef35bec76530b76563933bca
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-09-26 11:55:29 -0700

    :hammer: resolves #24

--- Songhay.Dashboard/ClientApp/src/app/routing.module.ts
+++ Songhay.Dashboard/ClientApp/src/app/routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { YouTubeModule } from './songhay/player/video/you-tube/you-tube.module';
+// TODO: see https://github.com/BryanWilhite/Songhay.Dashboard/issues/24
+// import { YouTubeModule } from './songhay/player/video/you-tube/you-tube.module';
 
 const routes: Routes = [
     { path: '', redirectTo: 'dash', pathMatch: 'full' },
     { path: 'dash', component: DashboardComponent },
-    { path: 'player/video/youtube', loadChildren: () => YouTubeModule }
+    { path: 'player/video/youtube', loadChildren: './songhay/player/video/you-tube/you-tube.module' }
 ];
 
 @NgModule({
