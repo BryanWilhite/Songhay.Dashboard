d48cf7e5976628493064a1772de9dde4c2caedca
Bryan Wilhite <bryan.wilhite@gmail.com>
2018-11-09 12:17:38 -0800

    :pencil2: added trap

--- Songhay.Dashboard.Shell/run.ps1
+++ Songhay.Dashboard.Shell/run.ps1
@@ -1,3 +1,9 @@
-﻿Set-Location $PSScriptRoot
+﻿trap 
+{ 
+    Write-Output $_ 
+    exit 1 
+}
+
+Set-Location $PSScriptRoot
 
 & dotnet Songhay.Dashboard.Shell.dll AppDataActivity --server-assembly-file 'D:\home\site\wwwroot\Songhay.Dashboard.dll'
