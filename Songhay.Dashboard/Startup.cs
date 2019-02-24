cf006f78ad99a83ab16226a9a9b82e4ce17c8ce2
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-24 09:58:34 -0800

    :tractor: :fire: resolves #49

--- Songhay.Dashboard/Startup.cs
+++ Songhay.Dashboard/Startup.cs
@@ -23,7 +23,7 @@ namespace Songhay.Dashboard
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "Client/dist";
             });
         }
 
@@ -54,7 +54,7 @@ namespace Songhay.Dashboard
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "Client";
 
                 if (env.IsDevelopment())
                 {
