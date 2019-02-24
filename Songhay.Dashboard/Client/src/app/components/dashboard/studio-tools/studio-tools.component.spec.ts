d426170dc7c40cf327c193ff51d8a6dc5311866d
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-02-24 08:50:36 -0800

    :umbrella: tests run as expected

--- Songhay.Dashboard/Client/src/app/components/dashboard/studio-tools/studio-tools.component.spec.ts
+++ Songhay.Dashboard/Client/src/app/components/dashboard/studio-tools/studio-tools.component.spec.ts
@@ -1,4 +1,4 @@
-import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StudioToolsComponent } from './studio-tools.component';
