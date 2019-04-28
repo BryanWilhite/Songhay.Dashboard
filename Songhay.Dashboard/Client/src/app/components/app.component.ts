64a96b747b512752c5e7f3061911d1eade26f32b
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-27 21:20:05 -0700

    :lipstick: :tractor: issue #52

--- Songhay.Dashboard/Client/src/app/components/app.component.ts
+++ Songhay.Dashboard/Client/src/app/components/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
@@ -8,11 +8,26 @@ import { MatIconRegistry } from '@angular/material';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
         iconRegistry.addSvgIconSetInNamespace(
             'rx',
             sanitizer.bypassSecurityTrustResourceUrl('assets/svg/sprites.svg')
         );
+    }
+
+    async ngAfterViewInit(): Promise<void> {
+        const timeout = (ms: number) => {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        };
+
+        await timeout(3000);
+
+        const main = Array.from(document.getElementsByTagName('main')).find(i => true) as HTMLMainElement;
+        main.classList.add('hidden');
+
+        await timeout(1000);
+
+        main.classList.add('collapsed');
     }
 }
