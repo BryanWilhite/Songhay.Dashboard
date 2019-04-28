ff11de4cecbc6f4aefd72b14ff0010f364afbfb2
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-04-28 10:56:25 -0700

    :hammer: :lipstick: :snowflake: resolves #52

--- Songhay.Dashboard/Client/src/app/components/app.component.ts
+++ Songhay.Dashboard/Client/src/app/components/app.component.ts
@@ -21,13 +21,28 @@ export class AppComponent implements AfterViewInit {
             return new Promise(resolve => setTimeout(resolve, ms));
         };
 
-        // await timeout(3000);
+        await timeout(3000);
 
-        // const main = Array.from(document.getElementsByTagName('main')).find(i => true) as HTMLMainElement;
-        // main.classList.add('hidden');
+        const main = Array.from(document.getElementsByTagName('main')).find(i => true) as HTMLMainElement;
 
-        // await timeout(1000);
+        if (!main) {
+            console.warn('The expected main splash element is not here.');
+            return;
+        }
 
-        // main.classList.add('collapsed');
+        const dialogClass = 'mdc-dialog--open';
+        const dialog = Array.from(main.children).find(i => i.classList.contains(dialogClass));
+
+        if (!dialog) {
+            console.warn('The expected main dialog splash element is not here.');
+            return;
+        }
+
+        dialog.classList.remove(dialogClass);
+        dialog.classList.remove('mdc-dialog--closed');
+
+        await timeout(100);
+
+        main.classList.add('collapsed');
     }
 }
