d73c44ec8d187bc378d610d59cc28bf29e18f902
Bryan Wilhite <bryan.wilhite@gmail.com>
2021-11-22 20:27:56 -0800

    #71 #72 🖼 🚜✨

--- Songhay.Dashboard.Server/Index.fs
+++ Songhay.Dashboard.Server/Index.fs
@@ -1,29 +1,42 @@
 module Songhay.Dashboard.Server.Index
 
-open Bolero
 open Bolero.Html
 open Bolero.Server.Html
 open Songhay.Dashboard
+open System
 
-let page = doctypeHtml [] [
-    head [] [
+let metaElements =
+    [
         meta [attr.charset "UTF-8"]
         meta [attr.name "viewport"; attr.content "width=device-width, initial-scale=1.0"]
-        title [] [text "Bolero Application"]
-        ``base`` [attr.href "/"]
-        link [attr.rel "stylesheet"; attr.href "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"]
-        link [attr.rel "stylesheet"; attr.href "css/index.css"]
     ]
-    body [] [
-        nav [attr.classes ["navbar"; "is-dark"]; "role" => "navigation"; attr.aria "label" "main navigation"] [
-            div [attr.classes ["navbar-brand"]] [
-                a [attr.classes ["navbar-item"; "has-text-weight-bold"; "is-size-5"]; attr.href "https://fsbolero.io"] [
-                    img [attr.style "height:40px"; attr.src "https://github.com/fsbolero/website/raw/master/src/Website/img/wasm-fsharp.png"]
-                    text "  Bolero"
-                ]
-            ]
-        ]
+
+let linkElements =
+    [
+        link [attr.rel "stylesheet"; attr.href "https://fonts.googleapis.com/icon?family=Material+Icons"]
+        link [attr.rel "stylesheet"; attr.href "https://fonts.googleapis.com/css?family=Roboto:300,400,500"]
+        link [attr.rel "stylesheet"; attr.href "css/index.min.css"]
+        link [attr.rel "icon"; attr.``type`` "image/x-icon"; attr.href "favicon.ico"]
+    ]
+
+let footerElement =
+    footer [] [
+        span [attr.classes ["copyright"]] [text $"© Bryan D. Wilhite {DateTime.Now.Year}"]
+    ]
+
+let page = doctypeHtml [] [
+    head [] (
+        metaElements
+        @
+        [``base`` [attr.href "/"]]
+        @
+        linkElements
+        @
+        [title [] [text "SonghaySystem(::)"]]
+    )
+    body [attr.classes ["mat-app-background"; "mat-typography"]] [
         div [attr.id "main"] [rootComp<Client.Main.MyApp>]
         boleroScript
+        footerElement
     ]
 ]
