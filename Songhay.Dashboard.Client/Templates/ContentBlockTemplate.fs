94880e0f3bd93526b4f41fbddb85cb1d611a4bfc
Bryan Wilhite <bryan.wilhite@gmail.com>
2021-12-05 20:22:49 -0800

    #74 🖼🚜✨

--- Songhay.Dashboard.Client/Templates/ContentBlockTemplate.fs
+++ Songhay.Dashboard.Client/Templates/ContentBlockTemplate.fs
@@ -4,11 +4,13 @@ open Bolero
 open Bolero.Html
 open Songhay.Dashboard.Client.ElmishTypes
 open Songhay.Dashboard.Client.Visuals
+open Songhay.Dashboard.Client.Templates.SvgSprites
 
 type ContentBlockTemplate = Template<"wwwroot/content-block.html">
 
 let viewMainTemplate model dispatch =
     ContentBlockTemplate()
+        .SvgSpritesNode(SvgSpritesTemplate().Elt())
         .StudioComponentNode(Tile.studioComponentNode)
         .Content(
             cond model.page <| function
---
+++ Songhay.Dashboard.Client/Templates/SvgSprites.fs
@@ -0,0 +1,5 @@
+module Songhay.Dashboard.Client.Templates.SvgSprites
+
+open Bolero
+
+type SvgSpritesTemplate = Template<"wwwroot/content-svg-sprites.html">
