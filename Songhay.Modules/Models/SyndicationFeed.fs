950ab5335850fe90160edb5bac38685e7d27b1f6
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-01-11 20:26:30 -0800

    #76 🚜✨🖋

---
+++ Songhay.Modules/Models/SyndicationFeed.fs
@@ -0,0 +1,28 @@
+﻿namespace Songhay.Modules.Models
+
+open System.Text.Json
+
+type SyndicationFeedItem =
+    {
+        title: string
+        link: string
+    }
+
+type SyndicationFeed =
+    {
+        feedImage: string option
+        feedItems: SyndicationFeedItem list
+        feedTitle: string
+    }
+
+    static member getAtomChannelTitle(rawFeed: JsonElement): string option =
+        Some System.String.Empty
+
+    static member getAtomChannelItems(rawFeed: JsonElement): SyndicationFeedItem list =
+        [{title = System.String.Empty; link = System.String.Empty}]
+
+    static member getRssChannelItems(rawFeed: JsonElement): SyndicationFeedItem list =
+        [{title = System.String.Empty; link = System.String.Empty}]
+
+    static member getRssChannelTitle(rawFeed: JsonElement): string option =
+        Some System.String.Empty
