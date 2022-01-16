27f78f8b2dcbd06684cf99b1e076373ad8d15749
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-01-15 21:52:12 -0800

    #76 🚜🖋🔬

--- Songhay.Modules.Tests/Models/SyndicationFeedTests.fs
+++ Songhay.Modules.Tests/Models/SyndicationFeedTests.fs
@@ -2,10 +2,30 @@ namespace Songhay.Modules.Models.Tests
 
 module SyndicationFeedTests =
 
-    open System
+    open System.IO
+    open System.Reflection
+    open System.Text.Json
     open Xunit
 
-    [<Fact>]
-    let ``My test`` () =
-        Assert.True(true)
+    open Songhay.Modules
+    open Songhay.Modules.Models
 
+    let projectDirectoryInfo =
+        Assembly.GetExecutingAssembly()
+        |> ProgramAssemblyInfo.getPathFromAssembly "../../../"
+        |> DirectoryInfo
+
+    let appJsonDocumentPath =
+        "./json/app.json"
+        |> ProgramFileUtility.getCombinedPath projectDirectoryInfo.FullName
+
+    let appJsonDocument = JsonDocument.Parse(File.ReadAllText(appJsonDocumentPath))
+
+    [<Fact>]
+    let ``app.json should have `feeds` property`` () =
+        let result =
+            match appJsonDocument.RootElement.TryGetProperty "feeds" with
+            | true, _ -> true
+            | _ -> false
+
+        Assert.True(result)
