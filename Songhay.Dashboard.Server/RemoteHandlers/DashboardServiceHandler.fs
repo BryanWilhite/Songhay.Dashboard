1168562461d0129887d8b5928a825a2675e36cad
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-02-19 17:02:05 -0800

    #77 added `IMemoryCache` 🔨

--- Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
+++ Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
@@ -1,20 +1,26 @@
 namespace Songhay.Dashboard.Server.RemoteHandlers
 
+open System
 open System.Net.Http
 open System.Threading.Tasks
+open Microsoft.Extensions.Caching.Memory
 open Microsoft.Extensions.Logging
 open Microsoft.FSharp.Core
 
 open Bolero.Remoting.Server
 
 open Songhay.Dashboard.Client.ElmishTypes
+open Songhay.Dashboard.Client.Models
 open Songhay.Dashboard.Client.SyndicationFeedUtility
 open Songhay.Modules.HttpClientUtility
 open Songhay.Modules.HttpRequestMessageUtility
 open Songhay.Modules.HttpResponseMessageUtility
 open Songhay.Modules.JsonDocumentUtility
+open Songhay.Modules.Models
 
-type DashboardServiceHandler(client: HttpClient, logger: ILogger<DashboardServiceHandler>) =
+type DashboardServiceHandler(client: HttpClient,
+                             logger: ILogger<DashboardServiceHandler>,
+                             cache: IMemoryCache) =
     inherit RemoteHandler<DashboardService>()
 
     static member getOutputResult response =
@@ -31,22 +37,30 @@ type DashboardServiceHandler(client: HttpClient, logger: ILogger<DashboardServic
                         | Result.Error _ -> None
                         | Result.Ok input -> input |> List.toArray |> Option.ofObj
         }
-
     override this.Handler =
         {
             getAppData = fun uri -> async {
-                logger.LogInformation($"calling {uri.OriginalString}...")
+                match cache.TryGetValue StudioFeedsPage with
+                | true, o ->
+                    return o :?> (FeedName * SyndicationFeed)[] option
+                | false, _ ->
+                    logger.LogInformation($"calling {uri.OriginalString}...")
 
-                let! responseResult =
-                    client
-                    |> trySendAsync (get uri)
-                    |> Async.AwaitTask
+                    let! responseResult =
+                        client
+                        |> trySendAsync (get uri)
+                        |> Async.AwaitTask
 
-                return!
-                    match responseResult with
-                    | Result.Error err ->
-                        logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
-                        Task.FromResult(None) |> Async.AwaitTask
-                    | Result.Ok response -> (DashboardServiceHandler.getOutputResult response) |> Async.AwaitTask 
+                    let! output =
+                        match responseResult with
+                        | Result.Error err ->
+                            logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
+                            Task.FromResult(None) |> Async.AwaitTask
+                        | Result.Ok response -> (DashboardServiceHandler.getOutputResult response) |> Async.AwaitTask
+
+                    let cacheEntryOptions = MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(5));
+                    cache.Set(StudioFeedsPage, output, cacheEntryOptions) |> ignore
+
+                    return output
             }
         }
