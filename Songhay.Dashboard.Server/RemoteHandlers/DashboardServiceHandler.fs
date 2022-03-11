82f6453d00d8e182138a2caadb4bd13ceda1d4ef
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-03-10 23:28:52 -0800

    #85 🔨🖋

--- Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
+++ Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
@@ -1,9 +1,9 @@
 namespace Songhay.Dashboard.Server.RemoteHandlers
 
 open System
+open System.Net
 open System.Net.Http
 open System.Text.Json
-open System.Threading.Tasks
 open Microsoft.Extensions.Caching.Memory
 open Microsoft.Extensions.Logging
 open Microsoft.FSharp.Collections
@@ -22,13 +22,13 @@ open Songhay.Modules.HttpResponseMessageUtility
 open Songhay.Modules.JsonDocumentUtility
 open Songhay.Modules.Models
 
-type DashboardServiceHandler(client: HttpClient,
-                             logger: ILogger<DashboardServiceHandler>,
-                             cache: IMemoryCache) =
+type DashboardServiceHandler(client: HttpClient, logger: ILogger<DashboardServiceHandler>, cache: IMemoryCache) =
     inherit RemoteHandler<DashboardService>()
 
     static member getOutputResultAsync (logger: ILogger) (responseResult: Result<HttpResponseMessage,exn>) =
         task {
+            logger.LogInformation("processing result...")
+
             match responseResult with
             | Result.Error err ->
                 logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
@@ -38,11 +38,21 @@ type DashboardServiceHandler(client: HttpClient,
 
                 let! jsonResult = response |> tryDownloadToStringAsync
 
-                return // TODO: use `logger` in `mapError`
+                return
                     jsonResult
-                    |> Result.mapError (fun code -> exn (code.ToString()))
+                    |> Result.mapError
+                           (
+                                fun code ->
+                                    logger.LogError($"The expected {nameof HttpStatusCode}, `{code},` is not here.")
+                                    exn (code.ToString())
+                           )
                     |> Result.bind (fun json -> json |> tryGetRootElement)
-                    |> Result.mapError (fun err -> JsonException err.Message)
+                    |> Result.mapError
+                           (
+                               fun err ->
+                                   logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
+                                   JsonException err.Message
+                            )
                     |> Result.bind (fun el -> el |> fromInput)
                     |> Result.map (fun input -> input |> List.toArray)
                     |> Option.ofResult
@@ -52,14 +62,16 @@ type DashboardServiceHandler(client: HttpClient,
         {
             getAppData = fun uri -> async {
                 match cache.TryGetValue StudioFeedsPage with
-                | true, o ->
-                    return o :?> (FeedName * SyndicationFeed)[] option
+                | true, o -> return o :?> (FeedName * SyndicationFeed)[] option
                 | false, _ ->
                     logger.LogInformation($"calling {uri.OriginalString}...")
 
                     let! responseResult = client |> trySendAsync (get uri) |> Async.AwaitTask
 
-                    let! output = responseResult |> (DashboardServiceHandler.getOutputResultAsync logger) |> Async.AwaitTask
+                    let! output =
+                        responseResult
+                        |> (DashboardServiceHandler.getOutputResultAsync logger)
+                        |> Async.AwaitTask
 
                     let cacheEntryOptions = MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(5));
                     cache.Set(StudioFeedsPage, output, cacheEntryOptions) |> ignore
