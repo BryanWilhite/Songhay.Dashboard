372c71cb2dae12f4d6f944268664c12701fe9d99
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-03-20 13:42:40 -0700

    #85 add `RemoteHandlerUtility.toHandlerOutputAsync`

--- Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
+++ Songhay.Dashboard.Server/RemoteHandlers/DashboardServiceHandler.fs
@@ -1,62 +1,26 @@
 namespace Songhay.Dashboard.Server.RemoteHandlers
 
 open System
-open System.Net
 open System.Net.Http
-open System.Text.Json
 open Microsoft.Extensions.Caching.Memory
 open Microsoft.Extensions.Logging
-open Microsoft.FSharp.Collections
 open Microsoft.FSharp.Core
 
 open FsToolkit.ErrorHandling
 
 open Bolero.Remoting.Server
 
-open Songhay.Dashboard.Client.ElmishTypes
-open Songhay.Dashboard.Models
-open Songhay.Dashboard.Client.SyndicationFeedUtility
 open Songhay.Modules.HttpClientUtility
 open Songhay.Modules.HttpRequestMessageUtility
-open Songhay.Modules.HttpResponseMessageUtility
-open Songhay.Modules.JsonDocumentUtility
 open Songhay.Modules.Models
+open Songhay.Modules.Bolero.RemoteHandlerUtility
+
+open Songhay.Dashboard.Models
+open Songhay.Dashboard.Client.ElmishTypes
+open Songhay.Dashboard.Server.DashboardServiceHandlerUtility
 
 type DashboardServiceHandler(client: HttpClient, logger: ILogger<DashboardServiceHandler>, cache: IMemoryCache) =
     inherit RemoteHandler<DashboardService>()
-
-    static member getOutputResultAsync (logger: ILogger) (responseResult: Result<HttpResponseMessage,exn>) =
-        task {
-            logger.LogInformation("processing result...")
-
-            match responseResult with
-            | Result.Error err ->
-                logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
-                return None
-
-            | Result.Ok response ->
-
-                let! jsonResult = response |> tryDownloadToStringAsync
-
-                return
-                    jsonResult
-                    |> Result.mapError
-                           (
-                                fun code ->
-                                    logger.LogError($"The expected {nameof HttpStatusCode}, `{code},` is not here.")
-                                    exn (code.ToString())
-                           )
-                    |> Result.bind (fun json -> json |> tryGetRootElement)
-                    |> Result.mapError
-                           (
-                               fun err ->
-                                   logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
-                                   JsonException err.Message
-                            )
-                    |> Result.bind (fun el -> el |> fromInput)
-                    |> Result.map (fun input -> input |> List.toArray)
-                    |> Option.ofResult
-        }
 
     override this.Handler =
         {
@@ -70,7 +34,7 @@ type DashboardServiceHandler(client: HttpClient, logger: ILogger<DashboardServic
 
                     let! output =
                         responseResult
-                        |> (DashboardServiceHandler.getOutputResultAsync logger)
+                        |> (toHandlerOutputAsync logger tryGetData)
                         |> Async.AwaitTask
 
                     let cacheEntryOptions = MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(5));
---
+++ Songhay.Modules.Bolero/RemoteHandlerUtility.fs
@@ -0,0 +1,26 @@
+namespace Songhay.Modules.Bolero
+
+open System.Net
+open System.Net.Http
+open System.Threading.Tasks
+open Microsoft.Extensions.Logging
+
+open Songhay.Modules.HttpResponseMessageUtility
+
+module RemoteHandlerUtility =
+    let toHandlerOutputAsync<'TOutput>
+        (logger: ILogger)
+        (dataGetter: ILogger -> Result<string, HttpStatusCode> -> 'TOutput option)
+        (responseResult: Result<HttpResponseMessage,exn>) : Task<'TOutput option> =
+        task {
+            logger.LogInformation("processing result...")
+
+            match responseResult with
+            | Result.Error err ->
+                logger.LogError(err.Message, err.GetType().FullName, err.Source, err.StackTrace)
+                return None
+
+            | Result.Ok response ->
+                let! jsonResult = response |> tryDownloadToStringAsync
+                return jsonResult |> dataGetter logger
+        }
