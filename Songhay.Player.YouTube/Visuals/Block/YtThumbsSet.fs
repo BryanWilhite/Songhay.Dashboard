8606b114be0714c937287d4518633d33abdf6b5f
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-04-15 21:53:14 -0700

    #85 add CSS for yt thumbs set overlay 💄🚜✨ start filling out `ytThumbsSetNode` 🔨

--- Songhay.Player.YouTube/Visuals/Block/YtThumbsSet.fs
+++ Songhay.Player.YouTube/Visuals/Block/YtThumbsSet.fs
@@ -4,14 +4,42 @@ open Microsoft.JSInterop
 
 open Bolero
 open Bolero.Html
-open Elmish
 
+open Songhay.Modules.Bolero.JsRuntimeUtility
 open Songhay.Player.YouTube
 open Songhay.Player.YouTube.Visuals.Block.YtThumbsNavigation
 
+[<Literal>] // see `Songhay.Player.YouTube/src/scss/you-tube-thumbs-set.scss`
+let CssAnimationNameBRollOverlayFadeIn = "b-roll-overlay-fade-in"
+
+[<Literal>] // see `Songhay.Player.YouTube/src/scss/you-tube-thumbs-set.scss`
+let CssAnimationNameBRollOverlayFadeOut = "b-roll-overlay-fade-out"
+
+[<Literal>] // see `$var-thumbs-overlay-animation-name` in `Songhay.Player.YouTube/src/scss/you-tube-thumbs-set.scss`
+let CssVarThumbsOverlayAnimationName = "--thumbs-overlay-animation-name"
+
 let ytThumbsSetNode (jsRuntime: IJSRuntime) (thumbsSetContainerRef: HtmlRef) (model: YouTubeModel) =
-    div
-        [ attr.classes [ "rx"; "b-roll"; "overlay" ] ; attr.ref thumbsSetContainerRef ]
-        [
-            bulmaDropdown jsRuntime model
-        ]
+    task {
+        if model.YtSetIsRequested then
+            let! playState = jsRuntime |> getComputedStylePropertyValueAsync thumbsSetContainerRef "animation-play-state"
+            jsRuntime |> consoleLogAsync [| {| playState = playState |} |] |> ignore
+    }
+    |> ignore
+
+    cond (model.YtSet.IsSome && model.YtSetIndex.IsSome) <| function
+    | true ->
+        div
+            [ attr.classes [ "rx"; "b-roll"; "overlay" ] ; attr.ref thumbsSetContainerRef ]
+            [
+                bulmaDropdown jsRuntime model
+            ]
+    | false ->
+        div
+            [ attr.classes [ "rx"; "b-roll"; "overlay" ] ; attr.ref thumbsSetContainerRef ]
+            [
+                div
+                    [ attr.classes [ "has-text-centered"; "p-6"] ]
+                    [
+                        div [ attr.classes [ "loader"; "m-6" ]; attr.title "Loading…" ] []
+                    ]
+            ]
