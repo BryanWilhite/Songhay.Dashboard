5b0cf96e7b70c476bc97fc0601a20f51bbec3f10
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-04-18 19:33:07 -0700

    #85 add Bulma dropdown CSS 💄

--- Songhay.Player.YouTube/Visuals/Block/YtThumbsNavigation.fs
+++ Songhay.Player.YouTube/Visuals/Block/YtThumbsNavigation.fs
@@ -1,29 +1,46 @@
 module Songhay.Player.YouTube.Visuals.Block.YtThumbsNavigation
 
+open System
 open Microsoft.JSInterop
 
 open Bolero.Html
 
 open Songhay.Player.YouTube
+open Songhay.Modules.Bolero.JsRuntimeUtility
 
 let bulmaDropdown (jsRuntime: IJSRuntime) (model: YouTubeModel) =
-    let clientId, segmentName, documents = model.YtSetIndex.Value
+    let _, segmentName, documents = model.YtSetIndex.Value
 
     div
-        [ attr.classes [ "dropdown"; "is-active" ] ]
+        [ attr.classes [ "dropdown" ] ]
         [
             div
                 [ attr.classes [ "dropdown-trigger" ] ]
                 [
                     button
-                        [ attr.classes [ "button" ] ; "aria-haspopup" => "true"; "aria-controls" => "dropdown-menu" ]
+                        [
+                            attr.classes [ "button" ]; "aria-haspopup" => "true"; "aria-controls" => "dropdown-menu"
+                            on.task.click (fun _ ->
+                                jsRuntime |> consoleLogAsync [| "click!" |]
+                            )
+                        ]
                         [
                             span [] [ text $"{segmentName.Value}" ]
                         ]
                 ]
             div
-                [ attr.classes [ "dropdown-menu" ] ; "role" => "menu" ]
+                [ attr.classes [ "dropdown-menu" ]; "role" => "menu" ]
                 [
+                    div
+                        [ attr.classes [ "dropdown-content" ] ]
+                        [
+                            forEach documents <| fun (display, _) ->
+                                if display.displayText.IsSome then
+                                    a
+                                        [ attr.classes [ "dropdown-item" ]; "data-id" => display.id.StringValue ]
+                                        [ text display.displayText.Value.Value ]
+                                else empty
+                        ]
                 ]
         ]
 
