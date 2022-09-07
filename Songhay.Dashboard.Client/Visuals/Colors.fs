bdcb136dac05733cb287afbcf161cc7ab4563839
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-09-06 20:27:43 -0700

    added `Colors` module 🚜✨

---
+++ Songhay.Dashboard.Client/Visuals/Colors.fs
@@ -0,0 +1,6 @@
+namespace Songhay.Dashboard.Client.Visuals
+
+module Colors =
+    let bulmaAkyinkyinBase = "is-akyinkyin-base"
+    let bulmaBackgroundGreyDarkTone = "has-background-greys-dark-tone"
+    let bulmaTextGreyLightTone = "has-text-greys-light-tone"
