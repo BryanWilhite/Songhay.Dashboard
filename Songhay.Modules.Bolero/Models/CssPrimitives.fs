453e1406871809f564be620dc64dd3ed3b8d84a1
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-09-13 20:10:03 -0700

    📖

--- Songhay.Modules.Bolero/Models/CssPrimitives.fs
+++ Songhay.Modules.Bolero/Models/CssPrimitives.fs
@@ -2,38 +2,55 @@ namespace Songhay.Modules.Bolero.Models
 
 open Bolero.Html
 
+///<summary>
+/// Identifies a list of <see cref="string" /> as a collection of CSS class names.
+///</summary>
+/// <remarks>
+/// The original inspiration for this type is based on
+/// the need to avoid writing <c>attr.``class``</c> more than once. 
+/// </remarks>
 type CssClasses =
+    //<summary> a list of <see cref="string" /> as a collection of CSS class names </summary>
     | CssClasses of string list
 
+    //<summary> converts a <see cref="string" /> into a CSS <c>class</c> <see cref="Attr" /> </summary>
     static member toHtmlClass (s: string) = (CssClasses [s]).ToHtmlClassAttribute
 
+    //<summary> converts a list of <see cref="string" /> into a CSS <c>class</c> <see cref="Attr" /> </summary>
     static member toHtmlClassFromList (list: string list) = (CssClasses list).ToHtmlClassAttribute
 
+    //<summary> returns the underlying list of <see cref="string" /> </summary>
     member this.Value = let (CssClasses l) = this in l
 
+    //<summary> appends the specified <see cref="string" /> to this instance </summary>
     member this.Append s = CssClasses (this.Value |> List.append([s]))
 
+    //<summary> appends the specified list of <see cref="string" /> to this instance </summary>
     member this.AppendList (l: string list) = CssClasses (this.Value |> List.append(l))
 
+    //<summary> pre-pends the specified <see cref="string" /> to this instance </summary>
     member this.Prepend s = CssClasses ([s] |> List.append(this.Value))
 
+    //<summary> pre-pends the specified list of <see cref="string" /> to this instance </summary>
     member this.PrependList (l: string list) = CssClasses (l |> List.append(this.Value))
 
+    //<summary> reduces the underlying list of <see cref="string" /> the the value of the <c>class</c> attribute </summary>
     member this.ToAttributeValue = this.Value |> List.reduce(fun a b -> $"{a} {b}")
 
+    //<summary> calls <see cref="ToAttributeValue" /> and return the result of <c>attr.``class``</c> </summary>
     member this.ToHtmlClassAttribute = attr.``class`` this.ToAttributeValue
 
 ///<summary>
 /// Enumerates a subset of the CSS inheritance values.
-/// </summary>
-/// <remarks>
+///</summary>
+///<remarks>
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/inheritance
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/inherit
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/initial
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/revert
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/revert-layer
 /// 📖 https://developer.mozilla.org/en-US/docs/Web/CSS/unset
-/// </remarks>
+///</remarks>
 type CssInheritance =
     ///<summary>
     /// “The inherit CSS keyword causes the element to take the computed value
