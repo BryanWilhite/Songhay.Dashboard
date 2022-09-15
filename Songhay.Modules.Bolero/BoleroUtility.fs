0dbaf3acc871edd6454b0870fba530ab074b4bb7
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-09-14 22:11:24 -0700

    📖

--- Songhay.Modules.Bolero/BoleroUtility.fs
+++ Songhay.Modules.Bolero/BoleroUtility.fs
@@ -5,10 +5,19 @@ open System
 open Bolero
 open Bolero.Html
 
+/// <summary>
+/// Utility functions mostly for <see cref="Node" />.
+/// </summary>
 module BoleroUtility =
 
+    /// <summary>
+    /// Wraps <see cref="Environment.NewLine" /> to format markup.
+    /// </summary>
     let newLine = rawHtml $"{Environment.NewLine}"
 
+    /// <summary>
+    /// Return spaces of indentation to format markup.
+    /// </summary>
     let indent level =
         let numberOfSpaces = 4
         let spaceChar = ' '
@@ -18,5 +27,9 @@ module BoleroUtility =
 
         String(charArray) |> text
 
+    /// <summary>
+    /// Inject a new line and indentation
+    /// between the specified list of <see cref="Node" />.
+    /// </summary>
     let wrapn indentLevel (nodes : Node list) =
         nodes |> List.collect (fun node -> [newLine; (indentLevel |> indent); node])
