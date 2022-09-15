003ffefddce517fcf6a22dda86160e498faebbe1
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-09-15 16:11:43 -0700

    📖

--- Songhay.Modules.Bolero/Visuals/Element.fs
+++ Songhay.Modules.Bolero/Visuals/Element.fs
@@ -8,8 +8,20 @@ open Bolero.Html
 
 open Songhay.Modules.Bolero.Models
 
+///<summary>
+/// Shared functions for generating HTML elements.
+///</summary>
+/// <remarks>
+/// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML
+/// </remarks>
 module Element =
 
+    ///<summary>
+    /// Returns the HTML anchor element, <c>a</c>, adorned with any CSS classes.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a
+    /// </remarks>
     let anchorElement (moreClasses: CssClassesOrEmpty) (href: Uri) (target: HtmlTargetOrEmpty) (childNodes: Node list) =
         a {
             moreClasses.Value
@@ -20,13 +32,26 @@ module Element =
             forEach childNodes <| id
         }
 
-    let buttonElement (callback: Web.MouseEventArgs -> unit) (cssClasses: CssClassesOrEmpty) =
+    ///<summary>
+    /// Returns the HTML <c>button</c> element,
+    /// declaring an eventing 🗲🐎 callback and adorned with any CSS classes.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
+    /// </remarks>
+    let buttonElement (cssClasses: CssClassesOrEmpty) (callback: Web.MouseEventArgs -> unit) =
         button {
             cssClasses.Value
 
             on.click callback
         }
 
+    ///<summary>
+    /// Returns the HTML <c>footer</c> element, adorned with any CSS classes.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/footer
+    /// </remarks>
     let footerElement (moreClasses: CssClassesOrEmpty) (childNodes: Node list) =
         footer {
             moreClasses.Value
@@ -34,22 +59,39 @@ module Element =
             forEach childNodes <| id
         }
 
+    ///<summary>
+    /// Returns an HTML comment.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started#html_comments
+    /// </remarks>
     let htmlComment (comment: string) = rawHtml $"<!-- {comment} -->"
 
+    ///<summary>
+    /// Returns the HTML image embed element, <c>img</c>, adorned with any CSS classes and any attributes.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img
+    /// </remarks>
     let imageElement
         (cssClasses: CssClassesOrEmpty)
         (moreAttrs: HtmlAttributesOrEmpty)
         (alt: string)
-        (uri: Uri) =
+        (src: Uri) =
         img {
             cssClasses.Value
 
-            attr.src uri.OriginalString
+            attr.src src.OriginalString
             attr.alt alt
             moreAttrs.Value
         }
 
-    ///<remarks>
+    ///<summary>
+    /// Returns the HTML paragraph element, <c>p</c>, adorned with any CSS classes and any attributes.
+    ///</summary>
+    /// <remarks>
+    /// 📖 https://developer.mozilla.org/en-US/docs/Web/HTML/Element/p
+    /// 
     /// Remember that the specified child <see cref="Node" /> can be <see cref="rawHtml" />.
     ///</remarks>
     let paragraphElement
