082b955d36523d3d6aaa21d0d9df278b1f42f737
Bryan Wilhite <bryan.wilhite@gmail.com>
2022-10-17 19:06:12 -0700

    ❄

--- Songhay.Dashboard.Activities/Assembly.cs
+++ Songhay.Dashboard.Activities/Assembly.cs
@@ -1,17 +1,13 @@
 global using System;
 global using System.Collections.Generic;
-global using System.Data;
-global using System.Data.Common;
 global using System.Diagnostics;
 global using System.IO;
 global using System.Linq;
-global using System.Net.Http;
 global using System.Threading.Tasks;
 global using Microsoft.Extensions.Configuration;
 global using Newtonsoft.Json;
 global using Newtonsoft.Json.Linq;
 global using Newtonsoft.Json.Serialization;
-global using Songhay;
 global using Songhay.Abstractions;
 global using Songhay.Diagnostics;
 global using Songhay.Extensions;
